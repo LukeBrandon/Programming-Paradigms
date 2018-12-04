50b4e3538569eb493adbb56b4540acee4dbbdcc8
LukeBrandon <lukebrandon68@gmail.com>
2018-12-04 15:39:20 -0600

    fully finished game

--- Python/View.py
+++ Python/View.py
@@ -10,7 +10,7 @@ class View():
 	keep_going = True
 
 	def __init__(self, model):
-		screen_size = (800,600)
+		screen_size = (1200,700)
 		self.screen = pygame.display.set_mode(screen_size)
 		pygame.display.set_caption("Mario")
 		self.turtle_image = pygame.image.load("images/turtle.png")
